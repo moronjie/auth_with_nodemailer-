e47bd60575549916c1b328063097048e2a680951
moronjie <njiemoro2@gmail.com>
2024-09-26 16:12:40 +0000

    auth

--- src/middleware/auth.jwt.ts
+++ src/middleware/auth.jwt.ts
@@ -9,22 +9,22 @@ export const authUser = async (req: AuthReq, res: Response, next: NextFunction) 
     // if(!req.headers.authorization) return next(customError("you are not authorized", 401))
 
     // const token = req.headers.authorization?.replace("Bearer ", "")
-    const token = req.cookies.token
-    if (!token) return next(customError("you are not authorized", 401))
+    const token = req.cookies.token;
+    if (!token) return next(customError("you are not authorized", 401));
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { _id: string }
-        if (!decoded) return next(customError("you are not authorized", 401))
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { _id: string };
+        if (!decoded) return next(customError("you are not authorized", 401));
     
-        const user = (await User.findById(decoded._id)) as userInterface
-        if (!user) return next(customError("User not found", 404))
-        user.password = ''
+        const user = (await User.findById(decoded._id)) as userInterface;
+        if (!user) return next(customError("User not found", 404));
+        user.password = '';
 
-        req.user = user
+        req.user = user;
 
-        next()
+        next();
     } catch (err) {
-        const error = err as Error
+        const error = err as Error;
         next(customError(error.message, 500));
     }
 
